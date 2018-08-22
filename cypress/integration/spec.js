3c4a923577a7f8b4201c098ed04f798f3587e6f2
Allison Hall <allison.d.hall@gmail.com>
2018-08-22 15:43:51 -0700

    Set up failing test scenario

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,20 @@
-describe('page', () => {
-  it('works', () => {})
-})
+describe('Non-deterministic test', () => {
+  beforeEach(() => {
+    cy.server();
+
+    cy.route({
+    	method: 'GET',
+    	url: '/todos/*'
+    }).as('testCall');
+  });
+
+  it('Trigger API call multiple times', () => {
+    cy.visit('/');
+
+    cy.contains('Make an API call').click();
+    cy.wait('@testCall'); // Fails here despite call appearing in Network tab
+
+    cy.contains('Make an API call').click();
+    cy.wait('@testCall');
+  });
+});
